2b21c1ef000494aae30fa5481a39d3105ffab241
Yu Tian <15968810047@163.com>
2023-04-04 17:35:29 +0800

    feat:新增html报告库,open_api接口

---
+++ interfaces/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+# @Author: yutian
+# @Version: Python 3.7
+# @File: __init__.py.py
+# @Date: 2023/4/4 16:52
+# @SoftWare: PyCharm
---
+++ test_cases/test_web/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+# @Author: yutian
+# @Version: Python 3.7
+# @File: __init__.py.py
+# @Date: 2023/4/4 17:34
+# @SoftWare: PyCharm
