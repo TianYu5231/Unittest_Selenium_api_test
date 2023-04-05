88d1e21240df07ed127ccbc4548f0e3f5ab4a36b
Yu Tian <15968810047@163.com>
2023-04-05 11:30:45 +0800

    feat:封住requests,修改log,增加suite

--- reports/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-# @Author: yutian
-# @Version: Python 3.7
-# @File: __init__.py.py
-# @Date: 2023/3/31 15:52
-# @SoftWare: PyCharm
---
+++ suites/api/test_open_api_suite.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# @Author: yutian
+# @Version: Python 3.7
+# @File: test_open_api_suite.py
+# @Date: 2023/4/5 10:25
+# @SoftWare: PyCharm
+import unittest
+
+from config import commons
+from test_cases.test_api.test_openapi import TestOpenApi
+
+cm = commons.Common()
+
+suite = unittest.TestSuite(unittest.TestLoader().loadTestsFromTestCase(TestOpenApi))
+cm.get_html_report(suite, title='OpenApi 测试报告')
